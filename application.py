a36a3320e744188cdb589b034e13b560ebd9b058
Gustavo Urizar <gustavo.urizar>
2015-05-11 15:46:47 -0600

    funtion to know the factorial number working

--- application.py
+++ application.py
@@ -1,3 +1,14 @@
-# Aqui escribe tu codigo
+"""Factorial"""
 
-print "Hello World"
+
+
+def factorial(number):
+	if number > 0:
+		fact = number * factorial(number -1)
+		return fact
+	else:
+		number == 0
+		return 1
+
+
+	
